725c5d3ce42147ffd1694db83db544ebbb950b95
Ashutosh Maurya <ashutoshmaurya9696@gmail.com>
2025-08-21 22:01:19 +0530

    Changed backend url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chat-app-backend-67t6.onrender.com/app"
+  baseURL: "https://chat-app-backend-67t6.onrender.com/app",
   //import.meta.env.MODE==="development"? "http://localhost:5000/app":"/app",
   withCredentials: true // ✅ important for cookies
 });
